6c5c7d5226f21a11beeea64d616f3127c8c562be
Nikola <nothignikola@email.com>
2023-06-22 23:07:56 +0200

    added v-if v-else router link

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,55 +1,71 @@
 <template>
-    <div id="home">
-      <nav>
-        <ul>
-          <li><router-link to="/main">Main page</router-link></li>
-          <li><router-link to="/profile">Profile</router-link></li>
-          <form @submit.prevent="search">
-            <input v-model="searchInput" type="text" placeholder="Search recipes">
-            <button type="submit">Search</button>
-          </form>
-        </ul>
-      </nav>
-      <div v-for="recipe in recipes" :key="recipe._id">
-        <h2>{{ recipe.title }}</h2>
-        <p>Created by: {{ recipe.createdBy.username }}</p>
-        <!-- ostali detalji recepata -->
-      </div>
+  <div id="home">
+    <nav>
+      <ul>
+        <li><router-link to="/main">Main page</router-link></li>
+        <li><router-link to="/profile">Profile</router-link></li>
+        <li>
+          <router-link v-if="isLoggedIn" to="/login" @click="logout">Logout</router-link>
+          <router-link v-else to="/login">Login</router-link>
+        </li>
+        <form @submit.prevent="search">
+          <input v-model="searchInput" type="text" placeholder="Search recipes">
+          <button type="submit">Search</button>
+        </form>
+      </ul>
+    </nav>
+    <div v-for="recipe in recipes" :key="recipe._id">
+      <h2>{{ recipe.title }}</h2>
+      <p>Created by: {{ recipe.createdBy.username }}</p>
     </div>
-  </template>
-  
-<script>
-export default {
-  data() {
-    return {
-      recipes: [],
-      searchInput: '',
-      isLoggedIn: !!localStorage.getItem('loginToken'), 
-    };
-  },
+  </div>
+</template>
 
-  async mounted() {
-    const response = await fetch('http://localhost:3000/recipe');
-    this.recipes = await response.json();
-  },
-  methods: {
-    async search() {
-      const response = await fetch(`http://localhost:3000/recipe/search?ingredients=${this.searchInput}`);
-      this.recipes = await response.json();
-    },
-    logout() {
-       const cookies = document.cookie.split(';');
+<script>
+import { ref, watchEffect } from 'vue'
+
+export default {
+  setup() {
+    const isLoggedIn = ref(!!localStorage.getItem('loginToken'))
+    const recipes = ref([])
+    const searchInput = ref('')
+
+    watchEffect(() => {
+      isLoggedIn.value = !!localStorage.getItem('loginToken')
+    })
+
+    const logout = () => {
+      const cookies = document.cookie.split(';');
 
       for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i];
         const eqPos = cookie.indexOf('=');
         const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
         document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/';
-  }
-}
+        localStorage.removeItem('loginToken');
+      }
+      isLoggedIn.value = false
+    }
 
+    return {
+      isLoggedIn,
+      recipes,
+      searchInput,
+      logout,
+     
+    }
   },
-};
-</script>
 
+  async mounted() {
+    const response = await fetch('http://localhost:3000/recipe');
+    this.recipes = await response.json();
+  },
   
+  methods: {
+    async search() {
+      const response = await fetch(`http://localhost:3000/recipe/search?ingredients=${this.searchInput}`);
+      this.recipes = await response.json();
+    },
+  },
+}
+</script>
